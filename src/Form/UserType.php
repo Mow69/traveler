f485502853cf0e86de1c3419ca025cc54412664d
Mouaz SAADAOUI <msa@cnam-lyon.peda>
2019-10-30 12:27:02 +0100

    templates in progress

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -14,18 +14,22 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $builder->getData();
         $builder
             ->add('login', TextType::class, [
                 'label' => "Nom d'utilisateur"
-            ])
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les mots de passe doivent être identiques',
-                'options' => ['attr' => ['class' => 'password-field']],
-                'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmez le mot de passe']
             ]);
+
+            if(is_null($user->getId())) {
+                $builder->add('password', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les mots de passe doivent être identiques',
+                    'options' => ['attr' => ['class' => 'password-field']],
+                    'required' => true,
+                    'first_options'  => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmez le mot de passe']
+                ]);
+            }
     }
 
     public function configureOptions(OptionsResolver $resolver)
