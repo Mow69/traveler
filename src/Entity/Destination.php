177e6e341eb227f64fe1c4f3a02e8dc66629a73d
Mouaz SAADAOUI <msa@cnam-lyon.peda>
2019-10-30 12:11:41 +0100

    templates in progress

---
+++ src/Entity/Destination.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DestinationRepository")
+ */
+class Destination
+{
+    use BaseEntityTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lng;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Pays", inversedBy="destinations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $pays;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Voyage", mappedBy="destination", orphanRemoval=true)
+     */
+    private $voyages;
+
+    public function __construct()
+    {
+        $this->voyages = new ArrayCollection();
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getLat(): ?string
+    {
+        return $this->lat;
+    }
+
+    public function setLat(?string $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLng(): ?string
+    {
+        return $this->lng;
+    }
+
+    public function setLng(?string $lng): self
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+
+    public function getPays(): ?Pays
+    {
+        return $this->pays;
+    }
+
+    public function setPays(?Pays $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Voyage[]
+     */
+    public function getVoyages(): Collection
+    {
+        return $this->voyages;
+    }
+
+    public function addVoyage(Voyage $voyage): self
+    {
+        if (!$this->voyages->contains($voyage)) {
+            $this->voyages[] = $voyage;
+            $voyage->setDestination($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVoyage(Voyage $voyage): self
+    {
+        if ($this->voyages->contains($voyage)) {
+            $this->voyages->removeElement($voyage);
+            // set the owning side to null (unless already changed)
+            if ($voyage->getDestination() === $this) {
+                $voyage->setDestination(null);
+            }
+        }
+
+        return $this;
+    }
+}
