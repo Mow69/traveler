e3bf7d5c1c839e7ecda682d72bf48ccf1d34533c
Mouaz SAADAOUI <msa@cnam-lyon.peda>
2019-10-31 14:33:26 +0100

    httpclient call OSM API by Service

--- src/Controller/Admin/DestinationController.php
+++ src/Controller/Admin/DestinationController.php
@@ -12,6 +12,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 /**
  * @Route("/destination")
@@ -43,9 +48,15 @@ class DestinationController extends AbstractController
     /**
      * @Route("/new", name="destination_new", methods={"GET","POST"})
      * @param Request $request
+     * @param OpenStreetMapAPIService $osmaService
      * @return Response
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
      */
-    public function new(Request $request): Response
+    public function new(Request $request, OpenStreetMapAPIService $osmaService): Response
     {
         // On définit une entité vide
         $destination = new Destination();
@@ -61,6 +72,18 @@ class DestinationController extends AbstractController
         // et que les données renseignées sont valides (bon format, dans les limites fixées, etc...)
         // on rentre dans le bloc d'instructions
         if ($form->isSubmitted() && $form->isValid()) {
+
+// Affecte dans un tableau
+            $tab = $osmaService->createConn($destination->getVille() . ', ' .$destination->getPays()->getNom());
+            //var_dump($tab);
+            $latitude = $tab[0]['lat'];
+            $longitude = $tab[0]['lon'];
+            $destination->setLat($latitude);
+            $destination->setLng($longitude);
+
+
+
+
             // Récupération du gestionnaire d'entités
             $entityManager = $this->getDoctrine()->getManager();
             // On indique au gestionnaire d'entités qu'on veut insérer $destination en BDD
@@ -85,6 +108,8 @@ class DestinationController extends AbstractController
 
     /**
      * @Route("/{id}", name="destination_show", methods={"GET"})
+     * @param Destination $destination
+     * @return Response
      */
     public function show(Destination $destination): Response
     {
@@ -133,20 +158,20 @@ class DestinationController extends AbstractController
         return $this->redirectToRoute('admin_destination_index');
     }
 
-// TODO: Faire la géolocalisation dans Controller
-
-    // CARTE
-    /**
-     * @Route("/carte", name="carte", methods={"GET"})
-     * @return JsonResponse
-     */
-    public function getMoviesList()
-    {
-        $osmAPIService = new OpenStreetMapAPIService();
-        $mapData = $osmAPIService->getAllMap();
-
-        return new JsonResponse($mapData, 200, [], true);
-    }
+//// TODO: Faire la géolocalisation dans Controller
+//
+//    // CARTE
+//    /**
+//     * @Route("/carte", name="carte", methods={"GET"})
+//     * @param OpenStreetMapAPIService $osmAPIService
+//     * @return JsonResponse
+//     */
+//    public function getMap(OpenStreetMapAPIService $osmAPIService)
+//    {
+//        $mapData = $osmAPIService->getAllMap();
+//
+//        return new JsonResponse($mapData, 200, [], true);
+//    }
 
 
 }
--- src/Service/OpenStreetMapAPIService.php
+++ src/Service/OpenStreetMapAPIService.php
@@ -4,63 +4,94 @@
 namespace App\Service;
 
 
+use Exception;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
 class OpenStreetMapAPIService
 {
-
+    /**
+     * @param $ville
+     * @return array
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws Exception
+     */
+    function createConn($ville)
+    {
+        $client = HttpClient::create(['http_version' => '2.0']);
+        $reponse = $client->request('GET', 'https://nominatim.openstreetmap.org/search/?format=json&limit=1&q=' . $ville);
+        if($reponse->getStatusCode() == 200){
+            $content = $reponse->toArray();
+            return $content;
+        } else {
+            throw new Exception('Bad connection my friend !!');
+        }
+    }
 
 
     // TODO: Faire la géolocalisation dans le Service
 
     // Récupérer les données de géolocalisation depuis l'API
-    /**
-     * @return bool|string
-     */
-    public function getAllMap()
-    {
-        return $this->callAPI("GET", "https://www.openstreetmap.org/#map=12/45.4315/4.4055");
-    }
+//    /**
+//     * @return bool|string
+//     */
+//    public function getAllMap()
+//    {
+//        return $this->callAPI("GET", "https://nominatim.openstreetmap.org/search.php?q=Lyon&limit=1&format=json");
+//    }
+//
+//    // Geolocalise une photo, une destination, un voyage...
+//    public function geolocalizeItem()
+//    {
+//
+//    }
+//
+//    // Appelle l'API
+//
+//    /**
+//     * @param $type
+//     * @param $url
+//     * @return bool|string
+//     */
+//    private function callAPI($type, $url)
+//    {
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => $url,
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => "",
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 30,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => $type,
+//            CURLOPT_HTTPHEADER => array(
+//                "Accept: */*",
+//                "Accept-Encoding: gzip, deflate",
+//                "Cache-Control: no-cache",
+//                "Connection: keep-alive",
+//                "Cookie: _osm_totp_token=974351",
+//                "Host: nominatim.openstreetmap.org",
+//                "Postman-Token: 4542991b-a5de-4d21-a1e9-a154b7c124f4,f8adf27c-c2a7-46ca-b75c-d63395fea9e3",
+//                "User-Agent: PostmanRuntime/7.19.0",
+//                "cache-control: no-cache"
+//            ),
+//        ));
+//
+//        $response = curl_exec($curl);
+//        curl_close($curl);
+//
+//        return $response;
+//    }
 
-    // Geolocalise une photo, une destination, un voyage...
-    public function geolocalizeItem()
-    {
-        
-    }
 
-    // Appelle l'API
-
-    /**
-     * @param $type
-     * @param $url
-     * @return bool|string
-     */
-    private function callAPI($type, $url)
-    {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => "",
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 30,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => $type,
-              CURLOPT_HTTPHEADER => array(
-                    "Accept: */*",
-                    "Accept-Encoding: gzip, deflate",
-                    "Cache-Control: no-cache",
-                    "Connection: keep-alive",
-                    "Cookie: _osm_session=ecae3ffe2e84df5c1a4f05b11fa7c29d; _osm_banner_sotm_asia_2019=1; _osm_totp_token=581977",
-                    "Host: www.openstreetmap.org",
-                    "Postman-Token: 36b88083-1b86-465e-afc7-b7a6469e0b97,8a59ba66-f3bb-4ef4-88d9-88c66153d35f",
-                    "User-Agent: PostmanRuntime/7.19.0",
-                    "cache-control: no-cache"
-              ),
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        return $response;
-    }
 
 }
